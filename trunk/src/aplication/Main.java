ff79b862bc7abf6a340814121ed1a3dc85528ee0
Your Name <you@example.com>
2015-12-28 20:07:38 +0600

    init

---
+++ trunk/src/aplication/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplication;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author vcosta
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Bem Vindo ao Projeto de LPII");
+
+    }
+
+}
